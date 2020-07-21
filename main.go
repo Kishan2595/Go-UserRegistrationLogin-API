9c96be4e93e372562c1cdd2c8558225752c66c1c
Kishan2595 <kishan95.kumar@gmail.com>
2020-07-21 08:46:33 +0530

    updating main

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/Kishan2595/Go-UserRegistrationLogin-API/controllers"
+	"github.com/Kishan2595/Go-UserRegistrationLogin-API/models"
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func Connectdb() {
 	r := gin.Default()
 
-	models.main()
+	models.Connectdb()
 
 	r.POST("r/egister", controllers.CreateUser)
 	r.POST("r/login", controllers.CreateUser)
